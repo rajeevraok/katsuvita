e81649596b59b8b2d3779bf3de3ec9434f252550
ALW-N <alwin@sjc.ac.in>
2022-06-23 11:20:43 +0530

    Adding Backend Files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const Mongo = require('./mongodb.js')
+var mongo = new Mongo();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+	res.sendFile(__dirname+"/index.html")
+})
+
+app.listen(5000,() =>{
+    console.log("Server hosted on port 5000")
+})
+    
